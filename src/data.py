32fa81451f0457383cce2f36033279191e97eacc
Rodrigo Marques <rodrigocanedomarques@hotmail.com>
2020-06-04 23:41:19 +0100

    add predecessor set to jobs rename ready parameter to in_degree

--- src/data.py
+++ src/data.py
@@ -1,15 +1,21 @@
 class Job:
     def __init__(self, id):
         self.id = id
-        self.duration = 0     # int
-        self.resources = []   # array of int (resource id)
-        self.nsuccessors = 0  # int
-        self.successors = []  # array of int (job id)
-        self.ready = 0        # ready == 0 when all predecessors are complete
-        
+        self.duration = 0        # int
+        self.resources = []      # array of int (resource id)
+        self.nsuccessors = 0     # int
+        self.successors = []     # array of int (job id)
+        self.npredecessors = 0   # int
+        self.predecessors = []   # array of int (job id)
+        self.in_degree = 0       # int
+        self.earliest_start = 0
+
+    def is_ready(self):
+        return self.in_degree == 0
+
     def __str__(self):
-        return "Job {}\nduration: {}\nresources: {}\nsuccessors: {}\nready:{}".format(
-            self.id, self.duration, self.resources, self.successors, self.ready)
+        return "Job {}\nduration: {}\nresources: {}\nsuccessors: {}\npredecessors: {}\nin_degree:{}".format(
+            self.id, self.duration, self.resources, self.successors, self.predecessors, self.in_degree)
 
 
 class Problem:
@@ -19,21 +25,22 @@ class Problem:
         self.nresources = 0     # int
         self.resources = []     # resources[i] = capacity of resource i
 
-        # switch to struct-of-arrays if original solution is too slow        
+        # switch to struct-of-arrays if original solution is too slow
         # self.durations      = [] # durations[j] = duration of job j
         # self.resource_uses  = [] # resource_uses[j] = resource usage of job j
         # self.nsuccessors    = [] # nsucessors[j] = number of successors for job j
         # self.successors     = [] # sucessors[j] = list of successors of job j
-        # self.ready          = [] # ready[j] == 0 if all predecessors of j are completed
+        # self.in_degree      = [] # in_degree[j] == 0 if all predecessors of j are completed
 
     def __str__(self):
         return "njobs:\t\t{}\nresources:\t{}\n".format(self.njobs, self.resources)
+
 
 def read_file(file):
     prob = Problem()
 
     with open(file, 'r') as f:
-        lines           = f.readlines();
+        lines = f.readlines()
 
         prob.njobs      = int(lines[5].split(":")[1])
         prob.nresources = int(lines[8].split(":")[1].split()[0])
@@ -45,7 +52,9 @@ def read_file(file):
         prob.resources = [int(r) for r in l]
 
         f.close()
-    
+
+    calc_predecessors(prob)
+
     return prob
 
 
@@ -54,18 +63,19 @@ def read_relations(prob, lines, job_start_line):
 
     for i in range(job_start_line, job_end_line):
         l = lines[i].split()
-        job            = Job(int(l[0]) - 1)
+        job = Job(int(l[0]) - 1)
         job.nsuccessors = int(l[2])
         job.successors  = [int(s) - 1 for s in l[-job.nsuccessors:]]
         prob.jobs.append(job)
-    
+
     prob.jobs.append(Job(prob.njobs - 1))
-    
+
     for j in prob.jobs:
         for succ in j.successors:
-            prob.jobs[succ].ready += 1
+            prob.jobs[succ].in_degree += 1
 
     return job_end_line
+
 
 def read_job_stats(prob, lines, resource_start_line):
     resource_end_line = resource_start_line - 1 + prob.njobs
@@ -73,7 +83,14 @@ def read_job_stats(prob, lines, resource_start_line):
     for i in range(resource_start_line, resource_end_line):
         l = lines[i].split()
         id = int(l[0]) - 1
-        prob.jobs[id].duration  = int(l[2])
+        prob.jobs[id].duration = int(l[2])
         prob.jobs[id].resources = [int(r) for r in l[-prob.nresources:]]
 
     return resource_end_line
+
+
+def calc_predecessors(prob):
+    for job in prob.jobs:
+        for j in job.successors:
+            prob.jobs[j].predecessors.append(job.id)
+
